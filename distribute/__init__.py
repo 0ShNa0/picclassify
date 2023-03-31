cf5614191a335b11b371eeccd1fef7b9ed655e9d
Shravani Nag <72717361+0ShNa0@users.noreply.github.com>
2023-03-31 23:07:25 +0530

    Add files via upload

--- distribute/__init__.py
+++ distribute/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,22 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implementation of the Keras API, the high-level API of TensorFlow.
-
-Detailed documentation and user guides are available at
-[keras.io](https://keras.io).
-"""
-from keras import distribute
-from keras import models
-from keras.engine.input_layer import Input
-from keras.engine.sequential import Sequential
-from keras.engine.training import Model
-
-# isort: off
-
-from tensorflow.python import tf2
-from tensorflow.python.util.tf_export import keras_export
-
-__version__ = "2.11.0"
-
-keras_export("keras.__version__").export_constant(__name__, "__version__")
+"""Keras' Distribution Strategy library."""
--- engine/saving.py
+++ engine/saving.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,22 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implementation of the Keras API, the high-level API of TensorFlow.
 
-Detailed documentation and user guides are available at
-[keras.io](https://keras.io).
+"""Model saving utilities.
+
+Everything has been moved to keras/saving/. This file will be deleted soon.
 """
-from keras import distribute
-from keras import models
-from keras.engine.input_layer import Input
-from keras.engine.sequential import Sequential
-from keras.engine.training import Model
 
-# isort: off
-
-from tensorflow.python import tf2
-from tensorflow.python.util.tf_export import keras_export
-
-__version__ = "2.11.0"
-
-keras_export("keras.__version__").export_constant(__name__, "__version__")
+from keras.saving import *  # noqa: F401,F403
